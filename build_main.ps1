8496a58efc0bf21c9049df453cc511941df301c9
JhonSmith0x7b <JhonSmith0x7b@github.com>
2024-01-22 17:47:58 +0800

    fix

--- build_main.ps1
+++ build_main.ps1
@@ -1,3 +1,6 @@
+pyinstaller -w -y ./qt_main.py
+
+
 Copy-Item ./.env -Destination ./dist/main -Force
 Copy-Item -Path ./prompts/ -Destination ./dist/main/prompts -Recurse -Force
 Copy-Item -Path ./resources/ -Destination ./dist/main/resources/ -Recurse -Force
