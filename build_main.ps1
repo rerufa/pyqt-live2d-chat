0657143aede25b46fef9fd1a8c94da2e37befc13
JhonSmith0x7b <JhonSmith0x7b@github.com>
2024-01-22 17:47:00 +0800

    add win build script

---
+++ build_main.ps1
@@ -0,0 +1,3 @@
+Copy-Item ./.env -Destination ./dist/main -Force
+Copy-Item -Path ./prompts/ -Destination ./dist/main/prompts -Recurse -Force
+Copy-Item -Path ./resources/ -Destination ./dist/main/resources/ -Recurse -Force
